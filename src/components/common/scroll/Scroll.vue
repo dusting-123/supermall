202e2456c7f8fce23702f2ce402d00558c6a7110
阿离 <zheng11564@163.com>
2021-08-25 17:57:52 +0800

    1

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="wrapper" ref="">
+    <div class="content">
+        <solt></solt>
+    </div>
+  </div>
+</template>
+
+<script>
+import bs from 'better-scroll'
+
+export default {
+  name:'Scroll',
+  data() {
+    return {
+
+    }
+  },
+  mounted(){
+
+  },
+  method:{
+
+  }
+}
+</script>
+
+<style>
+
+</style>
